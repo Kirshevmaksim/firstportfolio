9d09e56d4558ede7b177b278232c6a50b3e78707
Kirshevmaksim <66670144+Kirshevmaksim@users.noreply.github.com>
2020-06-18 15:49:35 +0300

    1
    Все готово.

--- проект шторы/js/index.js
+++ проект шторы/js/index.js
@@ -10,3 +10,15 @@ function ChangeFirstBlockImageAl() {
 	document.getElementById('AlButton').classList.add("activeButton");
 	document.getElementById('bumButton').classList.remove("activeButton");
 }
+function ChangeThirdBlockImageUniZebra() {
+	document.getElementById('Zebra').classList.add("hide");
+	document.getElementById('Uni-Zebra').classList.remove("hide");
+	document.getElementById('ZebraButton').classList.remove("activeButton");
+	document.getElementById('Uni-ZebraButton').classList.add("activeButton");
+}
+function ChangeThirdBlockImageZebra() {
+	document.getElementById('Zebra').classList.remove("hide");
+	document.getElementById('Uni-Zebra').classList.add("hide");
+	document.getElementById('ZebraButton').classList.add("activeButton");
+	document.getElementById('Uni-ZebraButton').classList.remove("activeButton");
+}
