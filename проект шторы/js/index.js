8d6006304a7a69279f5c1dcbad532b6464572ff8
Kirshevmaksim <66670144+Kirshevmaksim@users.noreply.github.com>
2020-06-19 16:33:51 +0300

    2
    Поработали с js

--- проект шторы/js/index.js
+++ проект шторы/js/index.js
@@ -22,3 +22,24 @@ function ChangeThirdBlockImageZebra() {
 	document.getElementById('ZebraButton').classList.add("activeButton");
 	document.getElementById('Uni-ZebraButton').classList.remove("activeButton");
 }
+function OpenPlicce(){
+	document.getElementById('plicce2').classList.remove("hide");
+	document.getElementById('plicce').classList.add("hide");
+}
+function ClosePlicce(){
+	document.getElementById('plicce2').classList.add("hide");
+	document.getElementById('plicce').classList.remove("hide");
+}
+var input1, input2, result;
+var prise=300;
+function MathCalc(){
+	if (input1>0 && input2>0) {
+	input1=document.getElementById("inp1").value;
+	input2=document.getElementById("inp2").value;
+	result=input1*input2*prise;
+	document.getElementById("res").innerHTML="Итого: "+result+" Dollars";
+	}
+	if (input1 != 0 || input2 != 0) {
+		document.getElementById("res").innerHTML="Вы ввели неверные значения"
+	}	
+}
